c747f81029fd5141493d39dd6f955b4ac7c54c02
mamazu <14860264+mamazu@users.noreply.github.com>
2025-02-19 13:24:23 +0100

    Adding server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
+import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import Menu from './components/Menu/Menu';
 
+const ws = new WebSocket('ws://localhost:3030/echo')
 function App() {
+    const [messages, setMessages] = useState<Array<string>>([]);
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+        ws.onmessage = (event) => {
+            console.log(event.data);
+            setMessages([...messages, event.data]);
+        }
+        ws.addEventListener('open', () => {
+            setReady(true);
+        });
+    }, []);
+
+    const stuff = useCallback(() => {
+        ws.send('Message');
+    }, []);
     return (
         <>
             <Menu />
-            Haallo
+            {ws.url} (Ready: {ready ? 'true' : 'false'})
+            <br />
+            <div>{messages.map(e => <p>{e}</p>)}</div>
+            <button onClick={stuff}>Send message</button>
         </>
     )
 }
